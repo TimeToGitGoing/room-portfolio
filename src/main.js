ba67fa5af46c166651e9dae604dca440555dd4aa
Huda <iamanotherday@gmail.com>
2025-03-11 13:35:34 +0200

    Added the scene and textures into the project

--- src/main.js
+++ src/main.js
@@ -13,14 +13,172 @@ const sizes = {
 // Loaders
 const textureLoader = new THREE.TextureLoader()
 
-// Model Loader
+// DRACO loader
 const dracoLoader = new DRACOLoader()
 dracoLoader.setDecoderPath('/draco/')
 
-const loader = new GLTFLoader()
-loader.setDRACOLoader(dracoLoader)
+// Textures
+const roomTex1 = textureLoader.load('/textures/room-tex-1.webp')
+const roomTex2 = textureLoader.load('/textures/room-tex-2.webp')
+const booksTex = textureLoader.load('/textures/books-tex.webp')
+const floorTex = textureLoader.load('/textures/floor-tex.webp')
+const monitorsTex = textureLoader.load('/textures/monitors-tex.webp')
+const nzTex = textureLoader.load('/textures/nz-tex.webp')
+const tuscanyTex = textureLoader.load('/textures/tuscany-tex.webp')
+const OuterWildsTex = textureLoader.load('/textures/Outerwilds-tex.webp')
+const pascal1Tex = textureLoader.load('/textures/Pascal1-tex.webp')
+const pascal2Tex = textureLoader.load('/textures/Pascal2-tex.webp')
+const pascal3Tex = textureLoader.load('/textures/pascal3-tex.webp')
+const plantTex = textureLoader.load('/textures/plant-tex.webp')
+const miscTex = textureLoader.load('/textures/misc-tex.webp')
+const shelfbookTex = textureLoader.load('/textures/shelf-book-tex.webp')
+const tableChairStuffTex = textureLoader.load('/textures/tablechairstuff-tex.webp')
 
-// const textureMap
+// GLTF loader
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+gltfLoader.load(
+  '/models/Room_Portfolio.glb',
+  (gltf) => {
+    gltf.scene.traverse((child)=>{
+      if (child.isMesh) {
+        if (child.name.includes("tex1")){
+          const material = new THREE.MeshBasicMaterial({
+            map: roomTex1
+          })
+          child.material = material
+        }
+        if (child.name.includes("tex2")){
+          const material = new THREE.MeshBasicMaterial({
+            map: roomTex2
+          })
+          child.material = material
+        }
+        if (child.name.includes("Books")){
+          const material = new THREE.MeshBasicMaterial({
+            map: booksTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("floor")){
+          const material = new THREE.MeshBasicMaterial({
+            map: floorTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("Monitors")){
+          const material = new THREE.MeshBasicMaterial({
+            map: monitorsTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("nz")){
+          const material = new THREE.MeshBasicMaterial({
+            map: nzTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("tuscany")){
+          const material = new THREE.MeshBasicMaterial({
+            map: tuscanyTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("outerwilds")){
+          const material = new THREE.MeshBasicMaterial({
+            map: OuterWildsTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("pascal1")){
+          const material = new THREE.MeshBasicMaterial({
+            map: pascal1Tex
+          })
+          child.material = material
+        }
+        if (child.name.includes("pascal2")){
+          const material = new THREE.MeshBasicMaterial({
+            map: pascal2Tex
+          })
+          child.material = material
+        }
+        if (child.name.includes("pascal3")){
+          const material = new THREE.MeshBasicMaterial({
+            map: pascal3Tex
+          })
+          child.material = material
+        }
+        if (child.name.includes("bigPlant")){
+          const material = new THREE.MeshBasicMaterial({
+            map: plantTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("Shelves")){
+          const material = new THREE.MeshBasicMaterial({
+            map: shelfbookTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("monitors")){
+          const material = new THREE.MeshBasicMaterial({
+            map: monitorsTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("Misc")){
+          const material = new THREE.MeshBasicMaterial({
+            map: miscTex
+          })
+          child.material = material
+        }
+        if (child.name.includes("otherStuff")){
+          const material = new THREE.MeshBasicMaterial({
+            map: tableChairStuffTex
+          })
+          child.material = material
+        }
+      }
+      
+    })
+    scene.add(gltf.scene)
+    console.log(gltf.scene)
+  }
+)
+
+// Materials
+roomTex1.flipY = false
+roomTex2.flipY = false
+booksTex.flipY = false
+floorTex.flipY = false
+monitorsTex.flipY = false
+nzTex.flipY = false
+tuscanyTex.flipY = false
+OuterWildsTex.flipY = false
+pascal1Tex.flipY = false
+pascal2Tex.flipY = false
+pascal3Tex.flipY = false
+shelfbookTex.flipY = false
+plantTex.flipY = false
+miscTex.flipY = false
+tableChairStuffTex.flipY = false
+
+roomTex1.colorSpace = THREE.SRGBColorSpace
+roomTex2.colorSpace = THREE.SRGBColorSpace
+booksTex.colorSpace = THREE.SRGBColorSpace
+floorTex.colorSpace = THREE.SRGBColorSpace
+monitorsTex.colorSpace = THREE.SRGBColorSpace
+nzTex.colorSpace = THREE.SRGBColorSpace
+tuscanyTex.colorSpace = THREE.SRGBColorSpace
+OuterWildsTex.colorSpace = THREE.SRGBColorSpace
+pascal1Tex.colorSpace = THREE.SRGBColorSpace
+pascal2Tex.colorSpace = THREE.SRGBColorSpace
+pascal3Tex.colorSpace = THREE.SRGBColorSpace
+shelfbookTex.colorSpace = THREE.SRGBColorSpace
+plantTex.colorSpace = THREE.SRGBColorSpace
+miscTex.colorSpace = THREE.SRGBColorSpace
+miscTex.colorSpace = THREE.SRGBColorSpace
+tableChairStuffTex.colorSpace = THREE.SRGBColorSpace
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 )
@@ -36,11 +194,6 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const controls = new OrbitControls( camera, renderer.domElement )
 controls.enableDamping = true
 controls.dampingFactor = 0.05
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 )
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
-const cube = new THREE.Mesh( geometry, material )
-scene.add( cube )
 
 // Event Listeners
 window.addEventListener("resize", () => {
@@ -58,9 +211,6 @@ window.addEventListener("resize", () => {
 
 const render = () => {
   controls.update()
-
-  cube.rotation.x += 0.01 
-  cube.rotation.y += 0.01
   
   renderer.render(scene, camera)
 
